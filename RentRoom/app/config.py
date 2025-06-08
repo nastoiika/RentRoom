4ec9a8873473c440effac4c15e75da5262e6e3e7
nastoika <146527663+nastoiika@users.noreply.github.com>
2025-06-08 13:55:25 +0300

    preparing to launch on the server

--- RentRoom/app/config.py
+++ RentRoom/app/config.py
@@ -1,6 +1,6 @@
 import os
 
-SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://admin:admin142%26hs@91.132.58.61:3306/rent_room'
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://Ivanova:u7o9p0p0q1q1@Ivanova.mysql.pythonanywhere-services.com/Ivanova$super_database"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = "f93d1a6b8f3d4b8e9c3a0d9f5a3c7e6d19e9e8c3a7b6d5f4e3c2a1b0f9e8d7c6"
 UPLOAD_FOLDER = 'static/uploads'
